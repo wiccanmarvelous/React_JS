a5bd5eafc50a0356f129564e5919f23f007c4860
Shreyash <shreyashyaduvanshi@gmail.com>
2023-10-02 20:02:29 +0530

    move 4 files, create 1065 files and update 2 files

--- 2023_10_01_tic-tac-toe/src/Components/TicTacToe/TicTacToe.jsx
+++ 2023_10_01_tic-tac-toe/src/Components/TicTacToe/TicTacToe.jsx
@@ -1,77 +1,75 @@
 import React, { useRef, useState } from 'react'
 import './TicTacToe.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark, faCircle } from '@fortawesome/free-solid-svg-icons'
+import cross_icon from '../Assets/cross.png';
+import circle_icon from '../Assets/circle.png';
 
 const TicTacToe = () => {
   const [count, setCount] = useState(0);
   const [lock, setLock] = useState(false);
   const [boxContents, setBoxContents] = useState(Array(9).fill(""));
   const titleRef = useRef(null);
-  let box1 = useRef(null);
-  let box2 = useRef(null);
-  let box3 = useRef(null);
-  let box4 = useRef(null);
-  let box5 = useRef(null);
-  let box6 = useRef(null);
-  let box7 = useRef(null);
-  let box8 = useRef(null);
-  let box9 = useRef(null);
-
-  let boxArray = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
+  const boxRefs = [useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null), useRef(null)];
 
   const toggle = (num) => {
     if (lock) {
       return;
     }
     if (boxContents[num] === "") {
-      // Create a copy of the entire boxContents array
       setBoxContents((prevBoxContent) => {
         const updatedBoxContents = [...prevBoxContent];
         updatedBoxContents[num] = count % 2 === 0 ? "x" : "o";
         return updatedBoxContents;
-      })
+      });
       setCount(count + 1);
       checkWin();
     }
   }
 
   const checkWin = () => {
-    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "")
-      won(data[2]);
-    else if (data[3] === data[4] && data[4] === data[5] & data[5] !== "")
-      won(data[5]);
-    else if (data[6] === data[7] && data[7] === data[8] & data[8] !== "")
-      won(data[8]);
-    else if (data[0] === data[3] && data[3] === data[6] & data[6] !== "")
-      won(data[6]);
-    else if (data[1] === data[4] && data[4] === data[7] & data[7] !== "")
-      won(data[7]);
-    else if (data[2] === data[5] && data[5] === data[8] & data[8] !== "")
-      won(data[8]);
-    else if (data[1] === data[4] && data[4] === data[8] & data[8] !== "")
-      won(data[8]);
-    else if (data[0] === data[1] && data[1] === data[2] & data[2] !== "")
-      won(data[2]);
-    else if (data[2] === data[4] && data[4] === data[6] & data[6] !== "")
-      won(data[6]);
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (const condition of winConditions) {
+      const [a, b, c] = condition;
+      if (boxContents[a] && boxContents[a] === boxContents[b] && boxContents[a] === boxContents[c]) {
+        won(boxContents[a]);
+        return;
+      }
+    }
+
+    // Check for a draw
+    if (!boxContents.includes("") && !lock) {
+      titleRef.current.innerHTML = 'It\'s a Draw!';
+      setLock(true);
+    }
   }
 
   const won = (winner) => {
     setLock(true);
-    if (winner === 'x')
-      titleRef.current.innerHtml = `Congratulations: <FontAwesomeIcon icon={faXmark} size="2xl" style={{ color: "#fff838" }} />`;
-    else
-    titleRef.current.innerHtml = `Congratulations: <FontAwesomeIcon icon={faCircle} size="2xl" style={{ color: "#fff838" }} />`;
+    if (winner === 'x') {
+      titleRef.current.innerHTML = 'Congratulations: X Wins!';
+    } else {
+      titleRef.current.innerHTML = 'Congratulations: O Wins!';
+    }
   }
 
   const reset = () => {
     setLock(false);
     setBoxContents(Array(9).fill(""));
-    titleRef.current.innerHtml = `Tic Tac Toe Game In <span>React</span>`
-    boxArray.map((e) => {
-      e.current.innerHtml = "";
-    })
+    titleRef.current.innerHTML = 'Tic Tac Toe Game In <span>React</span>';
+    boxRefs.forEach((boxRef) => {
+      if (boxRef.current) {
+        boxRef.current.innerHTML = "";
+      }
+    });
   }
 
   return (
@@ -80,30 +78,30 @@ const TicTacToe = () => {
       <div className="board">
         <div className="row1">
           {boxContents.slice(0, 3).map((content, index) => (
-            <div key={index} ref={`box${index + 1}`} className="boxes" onClick={() => toggle(index)}>
-              {content === "x" && <FontAwesomeIcon icon={faXmark} size="2xl" style={{ color: "#fff838" }} />}
-              {content === "o" && <FontAwesomeIcon icon={faCircle} size="2xl" style={{ color: "#005eff" }} />}
+            <div key={index} ref={boxRefs[index]} className="boxes" onClick={() => toggle(index)}>
+              {content === "x" && <img src={cross_icon} alt="X" />}
+              {content === "o" && <img src={circle_icon} alt="O" />}
             </div>
           ))}
         </div>
         <div className="row2">
           {boxContents.slice(3, 6).map((content, index) => (
-            <div key={index} ref={`box${index + 1}`} className="boxes" onClick={() => toggle(index)}>
-              {content === "x" && <FontAwesomeIcon icon={faXmark} size="2xl" style={{ color: "#fff838" }} />}
-              {content === "o" && <FontAwesomeIcon icon={faCircle} size="2xl" style={{ color: "#005eff" }} />}
+            <div key={index + 3} ref={boxRefs[index + 3]} className="boxes" onClick={() => toggle(index + 3)}>
+              {content === "x" && <img src={cross_icon} alt="X" />}
+              {content === "o" && <img src={circle_icon} alt="O" />}
             </div>
           ))}
         </div>
         <div className="row3">
           {boxContents.slice(6, 9).map((content, index) => (
-            <div key={index} ref={`box${index + 1}`} className="boxes" onClick={() => toggle(index)}>
-              {content === "x" && <FontAwesomeIcon icon={faXmark} size="2xl" style={{ color: "#fff838" }} />}
-              {content === "o" && <FontAwesomeIcon icon={faCircle} size="2xl" style={{ color: "#005eff" }} />}
+            <div key={index + 6} ref={boxRefs[index + 6]} className="boxes" onClick={() => toggle(index + 6)}>
+              {content === "x" && <img src={cross_icon} alt="X" />}
+              {content === "o" && <img src={circle_icon} alt="O" />}
             </div>
           ))}
         </div>
       </div>
-      <button className='reset' onClick={() => reset()}>Reset</button>
+      <button className='reset' onClick={reset}>Reset</button>
     </div>
   )
 }
